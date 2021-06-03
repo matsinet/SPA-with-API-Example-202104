7459439841ca0f02bddcda7a3699c929ec5bb1aa
matsinet <matsinet@gmail.com>
2021-06-02 21:59:29 -0500

    Express/Mongo 8.3

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
-const { request, response } = require("express");
+require("dotenv").config();
+const mongoose = require('mongoose');
 const express = require("express");
+
+const dbConnect = process.env.DB_CONNECT || "mongodb://localhost/pizzas";
+mongoose.connect(dbConnect);
 const app = express();
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', console.log.bind(console, 'Successfully opened connection to Mongo!'));
 // These lines are always at the top
 
 const myMiddleware = (request, response, next) => {
@@ -21,6 +29,24 @@ app.route("/")
     response.json(request.body);
   });
 
+// Contract of the data
+const pizzaSchema = new mongoose.Schema({
+  crust: String,
+  cheese: String,
+  sauce: String,
+  toppings: [String]
+});
+// Convert Schema a Model with CRUD operators
+const Pizza = mongoose.model('Pizza', pizzaSchema);
+
+// Create Route (post)
+app.post('/pizzas', (request, response) => {
+  const newPizza = new Pizza(request.body);
+  newPizza.save((err, pizza) => {
+    return err ? response.sendStatus(500).json(err) : response.json(pizza);
+  });
+});
+
 app.route("/pizzas/:id").get((request, response) => {
   // express adds a "params" Object to requests
   const id = request.params.id;
@@ -35,4 +61,5 @@ app.route("/**").get((request, response) => {
 });
 
 // This line is always LAST
-app.listen(4040, () => console.log("Listening on port 4040"));
+const PORT = process.env.PORT || 4040; 
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
